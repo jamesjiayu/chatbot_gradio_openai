9885f2378291b57b0d706cf4a8ade346a12f6393
Jiayu Wang <Labjwang2564@collin.edu>
2025-04-28 15:05:30 -0500

    openai 1st

---
+++ 1st_openai.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY") 
+
+client = OpenAI(api_key=api_key)
+
+response = client.responses.create(
+    model="gpt-4.1-nano-2025-04-14",
+    #"gpt-4.1",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
